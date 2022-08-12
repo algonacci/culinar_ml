17f5331823094d3a35aa0e6e18ff004590f70f7e
Zetta Eric Julianto <algonacci1@gmail.com>
2022-08-12 10:14:42 +0700

    :rocket: feat: Refactor and finishing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # culinar_ml
 The source code of ML service that empower culinar.in
+
+### Exploratory Data Analysis
+You can find how we find insights of the existing data [here](https://drive.google.com/file/d/1g403FcbSXYrVCCDes0DwONaQeUzNxQuw/view?usp=sharing)
+
+### We improve the existing dataset by adding image
+See the progress [here](https://drive.google.com/file/d/1eDi3KQN_ginQoatLW1XM2jzFTgP38qdB/view?usp=sharing)
---
+++ module.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+stop_words = set(stopwords.words('english'))
+stop_words.update(['.', ',', '"', "'", '?', '!', ':',
+                  ';', '(', ')', '[', ']', '{', '}'])
+
+food = pd.read_csv('food_data.csv', encoding='latin-1')
+resto = pd.read_csv('restaurant_data.csv', encoding='latin-1')
+resto = resto.loc[(resto['Country Code'] == 94) & (resto['City'] == 'Jakarta'), :]
+resto = resto.loc[resto['Longitude'] != 0, :]
+resto = resto.loc[resto['Latitude'] != 0, :]
+resto = resto.loc[resto['Latitude'] < 29]
+resto = resto.loc[resto['Rating text'] != 'Not rated']
+resto['Cuisines'] = resto['Cuisines'].astype(str)
+
+food_to_cuisine_map = {
+    "japanese": "japanese",
+    "korean": "korean",
+    "sunda": "sunda",
+    "indonesian": "indonesian",
+    "peranakan": "peranakan",
+    "burger": "burger",
+    "italian": "italian",
+    "café": "café",
+    "seafood": "seafood",
+    "western food": "western food",
+    "desserts": "desserts",
+    "bakery": "bakery",
+    "coffee and tea": "coffee and tea",
+    "cafe": "italian",
+    "ramen": "japanese",
+    "pizza": "pizza",
+}
+
+def search_comfort(mood):
+    lemmatizer = WordNetLemmatizer()
+    foodcount = {}
+    for i in range(124):
+        temp = [temps.strip().replace('.', '').replace(',', '').lower() for temps in str(
+            food["comfort_food_reasons"][i]).split(' ') if temps.strip() not in stop_words]
+        if mood in temp:
+            foodtemp = [lemmatizer.lemmatize(temps.strip().replace('.', '').replace(',', '').lower(
+            )) for temps in str(food["comfort_food"][i]).split(',') if temps.strip() not in stop_words]
+            for a in foodtemp:
+                if a not in foodcount.keys():
+                    foodcount[a] = 1
+                else:
+                    foodcount[a] += 1
+    sorted_food = []
+    sorted_food = sorted(foodcount, key=foodcount.get, reverse=True)
+    return sorted_food
+
+
+def find_my_comfort_food(mood):
+    topn = []
+    topn = search_comfort(mood)
+    return topn[:3]
